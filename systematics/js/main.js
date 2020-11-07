c3ce703f09005bcd4cd45bc7d9414927c3da8395
sazzatulislam <70699047+sazzatulislam@users.noreply.github.com>
2020-11-07 15:34:30 +0600

    Add files via upload

---
+++ systematics/js/main.js
@@ -0,0 +1,23 @@
+
+//bx slider 
+
+ $(document).ready(function() {
+     
+      $('#main_manu').meanmenu({
+          meanMenuContainer: '#mobile_menu',
+          meanScreenWidth: 991,
+      });
+     
+    });
+
+
+
+//menu bar
+
+
+
+
+
+
+
+
